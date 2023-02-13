fb79176701142c550f961ce4fd2199e34db7500e
OleksandrGnatiuk <oleksandr.gnatiuk@gmail.com>
2023-02-13 17:17:31 +0200

    changed README.md

--- active_list.py
+++ active_list.py
@@ -19,13 +19,16 @@ def create_active_list(dct) -> list[Card]:
 
 # Формуємо часові інтервали показу карток, в залежності від кількості їх попередніх показів
 intervals = {
-    1: timedelta(minutes=25),
-    2: timedelta(hours=8),
-    3: timedelta(days=1),
-    4: timedelta(days=3),
-    5: timedelta(days=10),
-    6: timedelta(days=30),
-    7: timedelta(days=90)
+    1: timedelta(minutes=1),
+    2: timedelta(minutes=2),
+    3: timedelta(minutes=10),
+    4: timedelta(hours=1),
+    5: timedelta(hours=5),
+    6: timedelta(days=1),
+    7: timedelta(days=5),
+    8: timedelta(days=25),
+    9: timedelta(days=120),
+    10: timedelta(days=730)
 }
 
 
@@ -33,7 +36,7 @@ def check_status_of_card(card: Card):
     time = datetime.now() - card.last_show
     if card.cnt_shows == 0:
         card.status = "active"
-    elif card.cnt_shows > 7 and time >= timedelta(days=90):
+    elif card.cnt_shows > 10 and time >= timedelta(days=90):
         card.status = "active"
     else:
         for cnt, interval in intervals.items():
